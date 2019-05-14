0a5c05d034bff975bca0f40f16a8c7879e6e6791
Artur <artur@itspace.am>
2019-05-15 00:22:17 +0400

    add client part

---
+++ web-socket-client/app/src/main/java/epsonsdk/example/com/androidwebsocket/MainActivity.java
@@ -0,0 +1,13 @@
+package epsonsdk.example.com.androidwebsocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
